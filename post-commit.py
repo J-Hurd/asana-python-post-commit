74292395a6959f7819b196c9e83e8490a72520e9
fhightower <floyd.hightower27@gmail.com>
2016-03-04 23:16:29 -0300

    added an MIT license to the file and did some pep cleanup

--- post-commit.py
+++ post-commit.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Floyd Hightower (https://github.com/fhightower)
-# 10.24.2015
-# This code was developed based on the bash script version available here:
-# https://github.com/Spaceman-Labs/asana-post-commit
-""" This is a python git hook (post-commit) script that closes a given asana task. """
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016 Floyd Hightower (https://github.com/fhightower)
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ----------------------------------------------------------------------------
+# Written: 10.24.2015
+
+"""This is a python git hook (post-commit) script that closes a given asana task.  This code was developed based on the bash script version available here: https://github.com/Spaceman-Labs/asana-post-commit
+"""
 
 import os
 import re
